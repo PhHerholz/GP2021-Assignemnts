654601d662718716cae34b26705b89038325f727
PhHerholz <ph.herholz@gmail.com>
2021-09-27 10:25:01 +0200

    Initial commit

---
+++ assignment3/src/main.cpp
@@ -0,0 +1,190 @@
+#include <iostream>
+#include <igl/readOFF.h>
+#include <igl/opengl/glfw/Viewer.h>
+#include <igl/opengl/glfw/imgui/ImGuiMenu.h>
+#include <igl/opengl/glfw/imgui/ImGuiHelpers.h>
+#include <imgui/imgui.h>
+/*** insert any libigl headers here ***/
+
+using namespace std;
+using Viewer = igl::opengl::glfw::Viewer;
+
+// Vertex array, #Vx3
+Eigen::MatrixXd V;
+// Face array, #Fx3
+Eigen::MatrixXi F;
+
+// Per-vertex uniform normal array, #Vx3
+Eigen::MatrixXd N_uniform;
+// Per-vertex area-weighted normal array, #Vx3
+Eigen::MatrixXd N_area;
+// Per-vertex mean-curvature normal array, #Vx3
+Eigen::MatrixXd N_meanCurvature;
+// Per-vertex PCA normal array, #Vx3
+Eigen::MatrixXd N_PCA;
+// Per-vertex quadratic fitted normal array, #Vx3
+Eigen::MatrixXd N_quadraticFit;
+
+// Per-vertex mean curvature, #Vx3
+Eigen::VectorXd K_mean;
+// Per-vertex Gaussian curvature, #Vx3
+Eigen::VectorXd K_Gaussian;
+// Per-vertex minimal principal curvature, #Vx3
+Eigen::VectorXd K_min_principal;
+// Per-vertex maximal principal curvature, #Vx3
+Eigen::VectorXd K_max_principal;
+// Per-vertex color array, #Vx3
+Eigen::MatrixXd colors_per_vertex;
+
+// Explicitely smoothed vertex array, #Vx3
+Eigen::MatrixXd V_expLap;
+// Bilateral smoothed vertex array, #Vx3
+Eigen::MatrixXd V_bilateral;
+
+bool callback_key_down(Viewer& viewer, unsigned char key, int modifiers) {
+    if (key == '1') {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        // Add your code for computing uniform vertex normals here:
+        // store in N_uniform
+
+        // Set the viewer normals.
+        viewer.data().set_normals(N_uniform);
+    }
+
+    if (key == '2') {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        // Add your code for computing area-weighted vertex normals here:
+        // store in N_area
+
+        // Set the viewer normals.
+        viewer.data().set_normals(N_area);
+    }
+
+    if (key == '3') {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        // Add your code for computing mean-curvature vertex normals here:
+        // store in N_meanCurvature
+
+        // Set the viewer normals.
+        viewer.data().set_normals(N_meanCurvature);
+    }
+
+    if (key == '4') {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        // Add your code for computing PCA vertex normals here:
+        // store in N_PCA
+
+        // Set the viewer normals.
+        viewer.data().set_normals(N_PCA);
+    }
+
+    if (key == '5') {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        // Add your code for computing quadratic fitted vertex normals here:
+        // store in N_quadraticFit
+
+        // Set the viewer normals.
+        viewer.data().set_normals(N_quadraticFit);
+    }
+
+    if (key == '6') {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        colors_per_vertex.setZero(V.rows(),3);
+        // Add your code for computing the discrete mean curvature
+        // store in K_mean
+
+        // Set the viewer colors
+        viewer.data().set_colors(colors_per_vertex);
+    }
+
+    if (key == '7') {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        colors_per_vertex.setZero(V.rows(),3);
+        // Add your code for computing the discrete Gaussian curvature
+        // store in K_Gaussian
+
+        // Set the viewer colors
+        viewer.data().set_colors(colors_per_vertex);
+    }
+
+    if (key == '8') {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        colors_per_vertex.setZero(V.rows(),3);
+        // Add your code for computing the discrete minimal principal curvature
+        // store in K_min_principal
+
+        // Set the viewer colors
+        viewer.data().set_colors(colors_per_vertex);
+    }
+
+    if (key == '8') {
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        colors_per_vertex.setZero(V.rows(),3);
+        // Add your code for computing the discrete maximal principal curvature
+        // store in K_min_principal
+
+        // Set the viewer colors
+        viewer.data().set_colors(colors_per_vertex);
+    }
+
+    if (key == 'E') {
+        // Add your code for computing explicit Laplacian smoothing here:
+        // store the smoothed vertices in V_expLap
+
+        // Set the smoothed mesh
+        viewer.data().clear();
+        viewer.data().set_mesh(V_expLap, F);
+    }
+
+    if (key == 'B') {
+        // Add your code for computing bilateral smoothing here:
+        // store the smoothed vertices in V_bilateral
+
+        viewer.data().clear();
+        viewer.data().set_mesh(V_bilateral, F);
+    }
+
+    return true;
+}
+
+bool load_mesh(Viewer& viewer,string filename, Eigen::MatrixXd& V, Eigen::MatrixXi& F)
+{
+  igl::readOBJ(filename,V,F);
+  viewer.data().clear();
+  viewer.data().set_mesh(V,F);
+  viewer.data().compute_normals();
+  viewer.core.align_camera_center(V, F);
+  return true;
+}
+
+int main(int argc, char *argv[]) {
+    // Show the mesh
+    Viewer viewer;
+    viewer.callback_key_down = callback_key_down;
+    
+    std::string filename;
+    if (argc == 2) {
+        filename = std::string(argv[1]);
+    }
+    else {
+        filename = std::string("../data/cow.obj");
+    }
+    load_mesh(viewer,filename,V,F);
+
+    callback_key_down(viewer, '1', 0);
+
+    // Attach a menu plugin
+    igl::opengl::glfw::imgui::ImGuiMenu menu;
+    viewer.plugins.push_back(&menu);
+    
+    viewer.launch();
+}
